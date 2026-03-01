da7f1188600624b94c0fc4d8779d047f1a5333f5
Vedant4117 <vedant.sc@somaiya.edu>
2026-03-01 18:44:23 +0530

    Registration connected with backend

--- nutrilink-backend/server.js
+++ nutrilink-backend/server.js
@@ -10,7 +10,11 @@ const beneficiaryRoutes = require("./routes/beneficiaryRoutes")
 const dashboardRoutes = require("./routes/dashboardRoutes");
 
 const app = express();
-const port = process.env.port
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
 
 app.use(cors())
 app.use(express.json())
