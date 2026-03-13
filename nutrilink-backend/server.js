ba1d97c60420fb2712c05fe9513daa8cc6e26475
riiddhi-c <riddhi.c107@gmail.com>
2026-03-13 12:44:25 +0530

    server.js issue

--- nutrilink-backend/server.js
+++ nutrilink-backend/server.js
@@ -8,6 +8,7 @@ const authRouters = require("./routes/authRoutes");
 const screeningRoutes = require("./routes/screeningRoutes");
 const beneficiaryRoutes = require("./routes/beneficiaryRoutes")
 const dashboardRoutes = require("./routes/dashboardRoutes");
+const followupRoutes = require("./routes/followupRouter");
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -27,6 +28,7 @@ app.use("/api/auth", authRouters);
 app.use("/api",screeningRoutes);
 app.use("/api",beneficiaryRoutes);
 app.use("/api", dashboardRoutes);
+app.use("/api", followupRoutes);
 
 app.listen(port, () => {
     console.log('Server running...!');
