7e131bf9ac6c74a56186aaa2b62b330c4809272f
riddhikale <riddhiskale28@gmail.com>
2026-04-01 20:12:58 +0530

    implement risk distribution calculation logic `

--- supervisor_analytics/risk_distribution.py
+++ supervisor_analytics/risk_distribution.py
@@ -1,1 +1,34 @@
 #calculates risk (hig/medium/low) for pie charts
+
+import pandas as pd
+
+def risk_distribution(data):
+
+    # Convert input data into dataframe
+    df = pd.DataFrame(data)
+
+    # Count each risk category
+    distribution = (
+        df["riskTag"]
+        .value_counts()
+        .reset_index()
+    )
+
+    # Rename columns
+    distribution.columns = ["riskLevel", "count"]
+
+    # Convert result back to JSON format
+    return distribution.to_dict(orient="records")
+
+#TESTING
+# if __name__ == "__main__":
+#
+#     sample_data = [
+#         {"riskTag":"High"},
+#         {"riskTag":"Medium"},
+#         {"riskTag":"High"},
+#         {"riskTag":"Low"},
+#         {"riskTag":"Medium"}
+#     ]
+#
+#     print(risk_distribution(sample_data))
